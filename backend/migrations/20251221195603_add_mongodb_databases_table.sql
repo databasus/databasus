347d4f3034a8f32eea27b01f3ba38b0d2ec6a479
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2026-01-15 15:19:54 +0800

    feat: add new fields to migration

--- backend/migrations/20251221195603_add_mongodb_databases_table.sql
+++ backend/migrations/20251221195603_add_mongodb_databases_table.sql
@@ -1,16 +1,19 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE mongodb_databases (
-    id            UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    database_id   UUID REFERENCES databases(id) ON DELETE CASCADE,
-    version       TEXT NOT NULL,
-    host          TEXT NOT NULL,
-    port          INT NOT NULL,
-    username      TEXT NOT NULL,
-    password      TEXT NOT NULL,
-    database      TEXT NOT NULL,
-    auth_database TEXT NOT NULL DEFAULT 'admin',
-    is_https      BOOLEAN NOT NULL DEFAULT FALSE
+    id                UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    database_id       UUID REFERENCES databases(id) ON DELETE CASCADE,
+    version           TEXT NOT NULL,
+    host              TEXT NOT NULL,
+    port              INT NOT NULL,
+    username          TEXT NOT NULL,
+    password          TEXT NOT NULL,
+    tls_ca_file       TEXT NOT NULL,
+    tls_cert_file     TEXT NOT NULL,  
+    tls_cert_key_file TEXT NOT NULL,
+    database          TEXT NOT NULL,
+    auth_database     TEXT NOT NULL DEFAULT 'admin',
+    is_https          BOOLEAN NOT NULL DEFAULT FALSE
 );
 -- +goose StatementEnd
 
